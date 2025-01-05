22ef4654e815536d1139667ed4c9d186b9e32ea5
Nicholas chuks <niccobery91@gmail.com>
2025-01-05 17:35:38 +0100

    dowloaded the google fonts

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Image from "../assets/404.gif";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  //redirect user to the previous page after 6 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(-1);
+    }, 6000);
+  });
+  return (
+    <section className="errorPage">
+      <div className="errorPage__container">
+        <img src={Image} alt="error_image" />
+        <h1>404</h1>
+        <p>
+          This page does not exist, You will be directed to the previous page
+          shortly.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
